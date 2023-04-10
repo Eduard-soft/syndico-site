5d5ff5710656a7086f0a53ef0a303277ac72624f
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2023-04-10 22:47:40 +0300

    Create project

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const header = () => {
+	return (
+		<header className={styles.header}>
+			<link>Home</link>
+		</header>
+	)
+}
+
+export default header
